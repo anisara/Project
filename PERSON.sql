4e22dc3c1dd41cd13642d0e7d6ddf4a188cc6725
anisara <anisara.kumpetch@gmail.com>
2015-12-17 15:14:08 +0700

    Revert "Project"
    This reverts commit 059de4d8334917abf9b8605e7526dc0d6f6eda7d.

--- PERSON.sql
+++
@@ -1,7 +0,0 @@
---------------------------------------------------------
---  File created - Tuesday-December-15-2015   
---------------------------------------------------------
-REM INSERTING into SYSTEM.PERSON
-SET DEFINE OFF;
-Insert into SYSTEM.PERSON (ID_P,NAME_P,SUR_P,BIRTH,SEX,TEL_P,SALARY,USERNAME,PASSWORD) values (1,'Praveerat','nitrathorn','21-06-1994','f','0999876542','568077','moii','111');
-Insert into SYSTEM.PERSON (ID_P,NAME_P,SUR_P,BIRTH,SEX,TEL_P,SALARY,USERNAME,PASSWORD) values (2,'Moiizie','Za','21-06-1994','female','0816787665','1000000000','moiizie','000');
